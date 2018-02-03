b0854f53700141a2fed52e89903759a06632930a
Daniel Alonso <danirhoads@gmail.com>
2018-02-03 12:10:02 +0100

    New tokenizer, token classifier

--- tokenizer.js
+++ tokenizer.js
@@ -1,12 +1,42 @@
-var tokenize = string => string.split(/,| |\n|\r/).reduce((acc, value) =>
-    value !== '' ? acc.concat(value) : acc, []);
+'use strict';
 
+const STOPPERS = [' ', ',', '\r', '\n'];
+const KEYWORDS = ['SELECT', 'FROM', 'WHERE', 'GROUP', 'ORDER', 'BY', 'LIMIT'];
 
-exports.tokenize = function (string) {
-  var tokens = tokenize(string);
-  var pointer = 0;
-  var hasNext = () => pointer < tokens.length;
-  var next = () => hasNext() ? tokens[pointer++] : null;
+const isStopper = c => STOPPERS.indexOf(c) !== -1;
+const isKeyword = t => KEYWORDS.indexOf(t) !== -1;
+const isNumber = t => !isNaN(parseFloat(t));
+const isString = t => t.charAt(0) === '"';
 
-  return { next: next, hasNext: hasNext };
+function* tokenGenerator (str) {
+  var c = '';
+
+  for (let i=0; i<str.length; i++) {
+    if (isStopper(str.charAt(i)) && c !== '') {
+      //t.push(c);
+      yield c;
+      c = '';
+    } else {
+      c += str.charAt(i);
+      if (i===str.length -1) yield c;
+    }
+  }
+}
+
+function classifyToken (token) {
+  var t = token.toUpperCase();
+  if (isKeyword(t)) return { value: token, type: 'KEYWORD' };
+  if (isNumber(t)) return { value: token, type: 'NUMBER' };
+  if (isString(t)) return { value: token, type: 'STRING' };
+  return { value: token, type: 'COLUMN' };
+}
+
+module.exports = function* (query) {
+  var t = tokenGenerator(query);
+  var n = t.next();
+
+  while (!n.done) {
+    yield classifyToken(n.value);
+    n = t.next();
+  }
 };
